e9676d1b92539bcdc9ace08cbbcf51dc96c024bc
miyaa <miyaa@www26145ue.sakura.ne.jp>
2013-03-19 02:16:38 +0900

    move directory

--- app/views/footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer">
-<a href="<?=BASE?>about"><?=_('About')?></a>
-<a href="<?=BASE?>terms"><?=_('Terms')?></a>
-<a href="<?=BASE?>privacy"><?=_('Privacy')?></a>
-<a href="<?=BASE?>help"><?=_('Help')?></a>
-<a href="<?=BASE?>contact"><?=_('Contact')?></a>
-<div id="copyright"><?php echo COPYRIGHT ?></div>
-</div>
--- app/views/list.php
+++
@@ -1,8 +0,0 @@
-<h1>ページ一覧</h1>
-<ul>
-<?php
-foreach($pages as $page) :
-?>
-<li><a href="<?=BASE?><?=$page['id']?>"><?=$page['title']?></a></li>
-<?php endforeach; ?>
-</ul>
