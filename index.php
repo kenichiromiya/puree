f4ffc3aea6c61281967ece750660028c46bcb434
miyaa <miyaa@www26145ue.sakura.ne.jp>
2013-03-22 19:30:40 +0900

    confディレクトリを変更

--- index.php
+++ index.php
@@ -4,13 +4,14 @@ if ($_POST['_method']) {
     $_SERVER["REQUEST_METHOD"] = strtoupper($_POST['_method']);
     unset($_POST['_method']);
 }
+$vendor = isset($_GET['vendor']) ? $_GET['vendor']."/" : "";
 $cwd = getcwd();
-set_include_path(get_include_path() . PATH_SEPARATOR . $cwd);
 set_include_path(get_include_path() . PATH_SEPARATOR . $cwd."/app");
 set_include_path(get_include_path() . PATH_SEPARATOR . $cwd."/lib");
 set_include_path(get_include_path() . PATH_SEPARATOR . $cwd."/app/views");
+set_include_path(get_include_path() . PATH_SEPARATOR . $cwd."/app/conf");
 //set_include_path(get_include_path() . PATH_SEPARATOR . "/var/www/html/puree/views/");
-include_once("config.php");
+include_once($vendor."define.php");
 include_once("Pure/Autoload.php");
 //include_once("Pure/MyPDO.php");
 //include_once("Pure/DB.php");
