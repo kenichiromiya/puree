e7170b458b188bd1148cbd218c2a6b0e97291939
miyaa <miyaa@www26145ue.sakura.ne.jp>
2013-04-07 22:07:31 +0900

    インクルードパスの読み込み方法変更

--- index.php
+++ index.php
@@ -7,10 +7,14 @@ if ($_POST['_method']) {
 }
 $vendor = isset($_GET['vendor']) ? $_GET['vendor']."/" : "";
 $cwd = getcwd();
+$include_path = array(get_include_path(),"$cwd/app","$cwd/lib","$cwd/app/views","$cwd/app/conf");
+set_include_path(implode(PATH_SEPARATOR,$include_path));
+/*
 set_include_path(get_include_path() . PATH_SEPARATOR . $cwd."/app");
 set_include_path(get_include_path() . PATH_SEPARATOR . $cwd."/lib");
 set_include_path(get_include_path() . PATH_SEPARATOR . $cwd."/app/views");
 set_include_path(get_include_path() . PATH_SEPARATOR . $cwd."/app/conf");
+*/
 //set_include_path(get_include_path() . PATH_SEPARATOR . "/var/www/html/puree/views/");
 include_once($vendor."define.php");
 include_once("Pure/Autoload.php");
